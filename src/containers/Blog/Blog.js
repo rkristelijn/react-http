36226205ea459d67efd5cdd6d485fce2647a6422
Remi Kristelijn <rkristelijn@gmail.com>
2018-09-28 11:17:31 +0200

    added get for blog posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+const DEBUG = true;
+
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    };
+
+    componentDidMount = () => {
+        if (DEBUG) console.log('Blog.js', 'componentDidMount');
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            if (DEBUG) console.log('Blog.js', 'response', response);
+            this.setState({ posts: response.data });
+        });
+    }
+    render = () => {
+        if (DEBUG) console.log('Blog.js', 'render');
+
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title}></Post>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
